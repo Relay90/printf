1d8ada9a612578ccc4630dd98f2fde2f738e0bb2
Relay90 <motaungr511@gmail.com>
2023-08-22 10:52:04 -0700

    printf

--- write_handlers.c
+++ write_handlers.c
@@ -12,7 +12,6 @@
  *
  * Return: Number of chars printed.
  */
-
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
 { /* char is stored at left and paddind at buffer's right */
@@ -58,7 +57,6 @@ int handle_write_char(char c, char buffer[],
  *
  * Return: Number of chars printed.
  */
-
 int write_number(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -93,7 +91,6 @@ int write_number(int is_negative, int ind, char buffer[],
  *
  * Return: Number of printed chars.
  */
-
 int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
 	int length, char padd, char extra_c)
@@ -152,7 +149,6 @@ int write_num(int ind, char buffer[],
  *
  * Return: Number of written chars.
  */
-
 int write_unsgnd(int is_negative, int ind,
 	char buffer[],
 	int flags, int width, int precision, int size)
@@ -212,7 +208,6 @@ int write_unsgnd(int is_negative, int ind,
  *
  * Return: Number of written chars.
  */
-
 int write_pointer(char buffer[], int ind, int length,
 	int width, int flags, char padd, char extra_c, int padd_start)
 {
